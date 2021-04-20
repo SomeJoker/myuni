3498f31c267b794b638ae8300f124ea6ee61af5e
Pengzhen Sun <sunpengzhen@wecash.net>
2021-04-20 20:37:41 +0800

    fix(Novaswap):change svg new photo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ import { useTranslation } from 'react-i18next'
 
 import styled from 'styled-components'
 
-import Logo from '../../assets/svg/logo2.svg'
+import Logo from '../../assets/svg/logo1.svg'
 import LogoDark from '../../assets/svg/logo_white.svg'
 import { useActiveWeb3React } from '../../hooks'
 import { useDarkModeManager } from '../../state/user/hooks'
